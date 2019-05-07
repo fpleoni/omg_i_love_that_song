8d221c309fd8c5cb849d346cce73b4bc5cd9f4c5
Florencia Leoni <florenleoni@gmail.com>
2019-05-07 09:18:43 -0400

    another API attempt

--- jupyter_notebooks/config.py
+++ jupyter_notebooks/config.py
@@ -1,2 +1,6 @@
 client_id = "f4a2c8d2f58d45f0a67c21e2cdf98373"
 client_secret = "79a5ce0b97a64cacb66641a6351d05e1" 
+
+last_fm_key = "91abb9f918f4aad2534ae77f9ac897f8"
+last_fm_secret = "4abced7727db298a70dfb99b0c31cd12"
+last_fm_user = "florenleoni"
